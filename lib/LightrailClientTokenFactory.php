521d2fb86fa5feb237f73b876d574c970db53d4f
Mohammad Jafari <mohammad.j@lightrail.com>
2017-10-30 10:28:49 -0700

    fixing a bug in reading validity period.

--- lib/LightrailClientTokenFactory.php
+++ lib/LightrailClientTokenFactory.php
@@ -5,7 +5,7 @@ namespace Lightrail;
 
 class LightrailClientTokenFactory {
 
-	public static function generate ($shopperId, $validityInMs) {
+	public static function generate ($shopperId, $validityInSeconds) {
 
 		if (!isset(Lightrail::$apiKey))
 			throw new BadParameterException("Lightrail.apiKey is not set.");
@@ -21,18 +21,20 @@ class LightrailClientTokenFactory {
 		);
 
 		$iat = time();
+
 		$token = array(
 			'shopperId' => $shopperId,
 			'iat' => $iat,
 			'g' => $gClaim
 		);
 
-		if (isset($validityInMs)) {
-			$exp = $iat + $validityInMs;
+		if (isset($validityInSeconds)) {
+			$exp = $iat + $validityInSeconds;
 			$token['exp']=$exp;
         }
 
 		$jwt = \Firebase\JWT\JWT::encode($token, Lightrail::$clientSecret, 'HS256');
 		return $jwt;
 	}
+
 }
